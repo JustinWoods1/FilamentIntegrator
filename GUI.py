824b27e2c5faca218ca1970a7478e4161ead5fc1
JustinWoods1 <Justin_Woods1@baylor.edu>
2022-05-04 16:11:07 -0500

    Final.2

--- GUI.py
+++ GUI.py
@@ -5,18 +5,18 @@ from time import sleep
 import math
 
 
-servo_pinL = 18
+#servo_pinL = 18
 #servo_pinR = 22
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servo_pinL, GPIO.OUT)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(servo_pinL, GPIO.OUT)
 #GPIO.setup(servo_pinR, GPIO.OUT)
-pwmL=GPIO.PWM(servo_pinL,50)
+#pwmL=GPIO.PWM(servo_pinL,50)
 #pwmR=GPIO.PWM(servo_pinR,50)
 
 ###################################FUNCTIONS
 def measure_button_press():
   measure_button.hide()
-  guide_button.hide()
+ # guide_button.hide()
   title.value="MEASUREMENT MENU"
   spool_title.show()
   spool_model.show()
@@ -25,35 +25,35 @@ def measure_button_press():
   mass_measurement.show()
   back_button.show()
 
-def fgs_button_press():
-  guide_button.hide()
-  measure_button.hide()
-  title.value="FILAMENT GUIDANCE SYSTEM MENU"
-  guidance_box.show()
-  fgs_on_button.show()
+#def fgs_button_press():
+ # guide_button.hide()
+ # measure_button.hide()
+ # title.value="FILAMENT GUIDANCE SYSTEM MENU"
+ # guidance_box.show()
+ # fgs_on_button.show()
 #  fgs_off_button.show()
-  back_button.show()
+ # back_button.show()
 
-def fgs_control():
-    pwmL.start(0)
+#def fgs_control():
+ #   pwmL.start(0)
     #pwmR.start(0)
-    dc = 0
-    speed=0.002
-    i=0
+  #  dc = 0
+  #  speed=0.002
+   # i=0
 
-    for i in range (1,100,1):
-      sleep(1) 
-      print("Enter while loop")
-      for dc in range(1,100,1):
-        pwmL.ChangeDutyCycle(10.3+(dc*0.0015))
-       # pwmR.ChangeDutyCycle(5.3-(dc*0.0015))
-        sleep(speed)
-      for dc in range(100,1,-1):
-        pwmL.ChangeDutyCycle(10.3+(dc*0.0015))
+   # for i in range (1,100,1):
+   #   sleep(1) 
+   #   print("Enter while loop")
+   #   for dc in range(1,100,1):
+   #     pwmL.ChangeDutyCycle(10.3+(dc*0.0015))
+   #    # pwmR.ChangeDutyCycle(5.3-(dc*0.0015))
+   #     sleep(speed)
+   #   for dc in range(100,1,-1):
+   #     pwmL.ChangeDutyCycle(10.3+(dc*0.0015))
         #pwmR.ChangeDutyCycle(5.3-(dc*0.0015))
-        sleep(speed)
-    pwmL.stop()
-    #pwmR.stop()
+   #     sleep(speed)
+   # pwmL.stop()
+   # #pwmR.stop()
 
 def chosen_spool():
   if spool_model.value   == "ESSENTIUM(Notch)":
@@ -85,14 +85,14 @@ def get_weight():
 def back_button_press():
   title.value="WELCOME, AML ASISTANT"
   measure_button.show()
-  guide_button.show()
+ # guide_button.show()
   back_button.hide()
   spool_title.hide()
   mass_title.hide()
   get_weight_button.hide()
-  guidance_box.hide()
+ # guidance_box.hide()
   mass_measurement.value = 0
-  pwmL.stop()
+ # pwmL.stop()
  # pwmR.stop()
 #MAIN
 	#main menu
@@ -101,8 +101,8 @@ title = Text(app, text="WELCOME, AML ASSISTANT!",grid=[1,1])
 bu_logo = Picture(app,image="logo_Baylor_University.png",grid=[0,0])
 measure_button = PushButton(app,text="MEASURE FUNCTION",pady=10,width=20,height=6,grid=[1,3],\
 	command=measure_button_press)
-guide_button   = PushButton(app,text="GUIDANCE FUNCTION",pady=10,width=20,height=6,grid=[1,5],\
-	command=fgs_button_press)
+#guide_button   = PushButton(app,text="GUIDANCE FUNCTION",pady=10,width=20,height=6,grid=[1,5],\
+	#command=fgs_button_press)
 #exit_button = PushButton(app, text="EXIT",grid=[0,6], command=servo_close)
 
 	#measure menu
@@ -122,11 +122,11 @@ mass_measurement = Text(mass_title,text=0,visible=False)
 back_button = PushButton(app,text="BACK", grid=[0,5],visible=False,command=back_button_press)
 
 	#guidance menu
-guidance_box = Box(app,layout="grid",grid=[1,2],visible=False)
-fgs_on_button = PushButton(guidance_box,text="ON",width=15,grid=[0,0],\
-	command=fgs_control,visible=False)
-fgs_on_button.bg=(0,255,0)
-fgs_on_button.text_size=18
+#guidance_box = Box(app,layout="grid",grid=[1,2],visible=False)
+#fgs_on_button = PushButton(guidance_box,text="ON",width=15,grid=[0,0],\
+#	command=fgs_control,visible=False)
+#fgs_on_button.bg=(0,255,0)
+#fgs_on_button.text_size=18
 
 #fgs_off_button = PushButton(guidance_box,text="OFF",width=15,grid=[0,2],\
 #	command=fgs_control,visible=False)
